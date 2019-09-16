0ff5710373c18d038fcffeacf163f68e8482a939
yarus <ank_work_box@mail.ru>
2019-09-16 18:32:31 +0300

    WwfScoreScreen added

---
+++ Wrestling.UI.Material/ScoreScreen/WwfScoreScreenView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Wrestling.UI.Material.Model;
+
+namespace Wrestling.UI.Material.ScoreScreen
+{
+    public partial class WwfScoreScreenView : PanelViewBase
+    {
+        public WwfScoreScreenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
