895e1e0abb510d28b20ae19035da8c354e3fb0d2
ONTARGET\akardash <alexey.kardash@ontarget-group.com>
2021-10-11 12:41:20 +0300

    Wrestling.Recorder.Config using VideoForge

---
+++ Wrestling.Recorder.Config/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace Wrestling.Recorder.Config
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IVideoCaptureAccessor, WpfVideoCaptureAccessor>();
+        }
+    }
+}
---
+++ Wrestling.Recorder.Config/OutputFormatInfoTag.cs
@@ -0,0 +1,13 @@
+﻿namespace Wrestling.Recorder.Config
+{
+    public enum OutputFormatInfoTag
+    {
+        AVI,
+        WMV,
+        MP4,
+        MP4_HW,
+        AGIF,
+        MPEGTS,
+        MOV,
+    }
+}
