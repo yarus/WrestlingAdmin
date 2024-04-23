004c99b1caf9ac0e0e0e12d2acc3b7c826a358a6
LAPTOP-TI9M0DMV\Aleksey Kardash <alexeykardash@gmail.com>
2024-04-23 14:52:27 +0300

    Fix scrolling issues in Applications and Draw screens Improve initial rendering performance in Applications and Draw screens Fix issue with wrestler moving from one group to another

--- Wrestling.UI.Material/Tournament/Dashboard/DashboardViewModel.cs
+++ Wrestling.UI.Material/Tournament/Dashboard/DashboardViewModel.cs
@@ -53,7 +53,7 @@ namespace Wrestling.UI.Material.Tournament.Dashboard
             SetupAutoSave();
         }
 
-        public override string PageTitle => "Вольная борьба - Администратор турниров версия 20231127";
+        public override string PageTitle => "Вольная борьба - Администратор турниров версия 20240423";
 
         public override IList<CommandButtonItem> QuickButtons
         {
